fd299fa933e131e7d8e31b87d1dc1af80d09933f
MoseyByrne <mosey.b.byrne@gmail.com>
2022-06-01 20:16:39 -0700

    add code example of a scope variable

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $("div#click-one").click(function() {
+    const whatToSay = "Hello!";
+    alert(whatToSay);
+  });
+
+  $("div#click-two").click(function() {
+    const whatToSay = "Bye now!";
+    alert(whatToSay);
+  });
+});
+
+
